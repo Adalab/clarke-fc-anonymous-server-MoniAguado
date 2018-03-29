f23eb664941ef62f7cb7ecb3946c4bbb94bac817
Moni Monroe <maguado85@gmail.com>
2018-03-29 16:29:21 +0200

    styles and try clic but doesn't work

---
+++ js/main.js
@@ -0,0 +1,27 @@
+'use strict';
+
+// MENU
+
+var buttonOpen = document.querySelector('.nav-trigger');
+var menu = document.querySelector('.page');
+var buttonClose = document.querySelector('.nav-close');
+
+function openMenu() {
+  menu.classList.add('menu-visible');
+}
+
+function closeMenu() {
+  menu.classList.remove('menu-visible');
+}
+
+buttonOpen.addEventListener('click', openMenu);
+buttonClose.addEventListener('click', closeMenu);
+
+
+var links = document.querySelectorAll('.nav-link');
+for (var i = 0; i < links.length; i++) {
+	links[i].addEventListener('click', closeMenu);
+}
+
+var click = document.querySelector('.page');
+click.addEventListener('click', closeMenu);
